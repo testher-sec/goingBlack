b2967dd9168f14d91cecd7c37af15cb197ee264f
Neska Sec <testher.sec@gmail.com>
2019-01-01 19:43:13 +0100

    add icmp header parsing

--- contents2/snifferICMP.py
+++ contents2/snifferICMP.py
@@ -1,9 +1,24 @@
-from ctypes import Structure, c_ubyte, c_ushort, c_uint32
+from ctypes import Structure, c_ubyte, c_ushort, c_uint32, sizeof
 import socket
 import struct
 import os
 
 host = "127.0.0.1"
+
+class ICMP(Structure):
+    _fields_ = [
+        ("type", c_ubyte),
+        ("code", c_ubyte),
+        ("checksum", c_ushort),
+        ("unused", c_ushort),
+        ("next_hop_mtu", c_ushort),
+    ]
+
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        pass
 
 class IP(Structure):
     _fields_ = [
@@ -60,6 +75,18 @@ try:
         ip_header = IP(raw_buffer[:20])
         #print out the protocol that was detected and the hosts
         print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+
+        # if it is ICMP we want it
+        if ip_header.protocol == "ICMP":
+
+            # calculate where our ICMP packet starts
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+            # create our ICMP structure
+            icmp_header = ICMP(buf)
+
+            print "ICM -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code)
 except KeyboardInterrupt:
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)

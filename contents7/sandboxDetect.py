7e3eb44fce10b82f165682b4ff088811e47a4575
Neska Sec <testher.sec@gmail.com>
2019-01-18 14:02:12 +0100

    Nice... detecting last input information

---
+++ contents7/sandboxDetect.py
@@ -0,0 +1,41 @@
+import ctypes
+import random
+import time
+import sys
+
+user32 = ctypes.windll.user32
+kernel32 = ctypes.windll.kernel32
+
+keystrokes = 0
+mouse_clicks = 0
+double_clicks = 0
+
+
+class LASTINPUTINFO(ctypes.Structure):
+    _fields_ = [ ("cbSize", ctypes.c_uint),
+                 ("dwTime", ctypes.c_ulong)]
+
+def get_last_input():
+    struct_lastinputinfo = LASTINPUTINFO()
+    struct_lastinputinfo.cbSize = ctypes.sizeof(LASTINPUTINFO)
+
+    # get last input registered
+    user32.GetLastInputInfo(ctypes.byref(struct_lastinputinfo))
+
+    # now determine how long the machine has been running
+    run_time = kernel32.GetTickCount()
+
+    elapsed = run_time - struct_lastinputinfo.dwTime
+
+    print "[*] It's been %d milliseconds since the last input event." % elapsed
+
+    return elapsed
+
+# Test Code
+get_last_input()
+time.sleep(1)
+get_last_input()
+time.sleep(1)
+get_last_input()
+time.sleep(1)
+get_last_input()
